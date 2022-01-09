0ded23895d8403fc8a5affcf841e773ce37bbf2f
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2022-01-09 15:50:17 -0300

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ import Main from './pages/Main'
 import NewClient from './pages/NewClient'
 import EditClient from './pages/EditClient'
 import ShowClient from './pages/ShowClient'
+import Home from './pages/Home'
 
 function App() {
-
-  console.log(import.meta.env)
 
   return (
     <BrowserRouter>
       <Routes>
-
+      <Route path="/" element={<Home />} />
         <Route path="/clients" element={<Layout/>}>
           <Route index element={<Main />} />
           <Route path="new" element={<NewClient />} />
           <Route path="edit/:id" element={<EditClient />} />
           <Route path=":id" element={<ShowClient />} />
-        </Route>
-
+         </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Layout from "../layout/Layout"
+
+const Home = () => {
+
+    return(
+        <div>
+            <h1>Welcome to CRM APP</h1>
+        </div>
+    )
+}
+
+export default Home
